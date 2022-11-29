05fea4f8d13d48bc8355e2b5432a3f5a4e2de44b
pallavi <115393023+Pallavikumari8@users.noreply.github.com>
2022-11-30 01:07:00 +0530

    Create class13.py

---
+++ class13.py
@@ -0,0 +1,12 @@
+#**print these following lines ***
+'''this is \\ double slash
+these are /\/\/\/\ mountains
+he is   awesome (use escape )
+\" \n \t \'(print this as an output)
+'''
+
+print("\\")
+print("Pallavi is\tawesome girl")
+print("Pallavi\"")
+print("Pallavi\t")
+print("Pallavi\nKumari")
