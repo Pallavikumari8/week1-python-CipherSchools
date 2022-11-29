38b9eadc4895bb22bdb5b1d0be95a186891deaae
pallavi <115393023+Pallavikumari8@users.noreply.github.com>
2022-11-30 00:43:37 +0530

    Create class10.py

---
+++ class10.py
@@ -0,0 +1,13 @@
+print("hello \"world\" world")
+print('I\'m Pallavi')
+
+print("line A")
+print("line B")
+print("line A\nline B\n line C")
+
+print("name\tPallavi")
+
+print("this is backslash\\")
+print("this is double backslash\\\\")
+
+print("hell\bro")
